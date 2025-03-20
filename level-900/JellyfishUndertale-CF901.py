7031bc324779a1068610505a930eedd406201877
sid717 <siddhantbakshi1979@gmail.com>
2025-03-21 01:40:49 +0530

    solved some shi

--- level-900/JellyfishUndertale-CF901.py
+++ level-900/JellyfishUndertale-CF901.py
@@ -1,4 +1,4 @@
-from io import StringIO
+# from io import StringIO
 import sys
 
 def main():
@@ -10,26 +10,28 @@ def main():
     index += 1
 
     for _ in range(t):
-        a, b, n = map(int, data[index].split()) #for individual values
+        a, b, n = map(int, data[index].split())
         index += 1
-        numbers = list(map(int, data[index].split())) #for inputting into an array
+        numbers = list(map(int, data[index].split()))
         index += 1
+        
+        result = b-1 if b > 1 else 0
+        b = 1
 
-        result = 0
         for i in range(n):
-            if numbers[i] + b >= a:
-                result += 1
-
-        sys.stdout.write(str(result) + "\n")
+            b = min(a, numbers[i] + 1) - 1
+            result += b
+        
+        sys.stdout.write(str(result + 1) + "\n")
 
 
 if __name__ == "__main__":
-    test_input = """2
-5 3 3
-1 1 7
-7 1 5
-1 2 5 6 8
+#     test_input = """2
+# 5 3 3
+# 1 1 7
+# 7 1 5
+# 1 2 5 6 8
 
-    """
-    sys.stdin = StringIO(test_input)
+#     """
+#     sys.stdin = StringIO(test_input)
     main()
--- level-900/MakeItZero-CF896.py
+++ level-900/MakeItZero-CF896.py
@@ -10,24 +10,18 @@ def main():
     index += 1
 
     for _ in range(t):
-        n = int(data[index])
+        n = int(data[index]) #for individual value
+        index += 1
+        numbers = list(map(int, data[index].split())) #for inputting into an array
         index += 1
 
-        steps = 1
-        while (n > 3):
-            n //= 4
-            steps *= 2
+        
 
-        sys.stdout.write(str(steps) + "\n")
+        sys.stdout.write(str(result) + "\n")
 
 
 if __name__ == "__main__":
-    test_input = """4
-1
-5
-16
-1000000000000000000
-
+    test_input = """
     """
     sys.stdin = StringIO(test_input)
     main()
